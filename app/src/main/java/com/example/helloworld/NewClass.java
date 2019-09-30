10f259324d454ee894ff64e5cb19219653fc5f15
Kenneth Maiyo <keimayo@students.uonbi.ac.ke>
2019-09-30 11:58:04 +0300

    New Java class

--- app/src/main/java/com/example/helloworld/NewClass.java
+++ app/src/main/java/com/example/helloworld/NewClass.java
@@ -1,6 +1,14 @@
 package com.example.helloworld;
 
+import androidx.annotation.NonNull;
+
 public class NewClass {
     public NewClass() {
+    }
+
+    @NonNull
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
     }
 }
